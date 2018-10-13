39a0f9c7f3628e4f6e72495fcedbc8f9dd368274
pujianto131 <pujianto.e131@gmail.com>
2018-10-14 00:37:50 +0700

    firstCommit

---
+++ src/messenger/Messenger.java
@@ -0,0 +1,22 @@
+
+package messenger;
+
+
+import messenger.Ui.JFrameLogIn;
+
+/**
+ *
+ * @author eko
+ */
+public class Messenger{
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        JFrameLogIn mFrameLogIn = new JFrameLogIn();
+        mFrameLogIn.setVisible(true);
+        
+    }
+    
+}
